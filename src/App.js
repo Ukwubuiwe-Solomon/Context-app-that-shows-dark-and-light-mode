e520c5bf28fa2486c7c6ac849a192ea5b5e98a65
lotfy669 <M.lotfy9966@gmail.com>
2023-02-17 21:29:08 +0200

    check git file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 
 import React, { useContext } from "react";
-import  About  from "./Components/About/About";
+import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
-import  Intro  from "./Components/Intro/Intro";
-import  ProductList  from "./Components/ProductList/productList";
+import Intro from "./Components/Intro/Intro";
+import ProductList from "./Components/ProductList/productList";
 import Toggle from "./Components/Toggle/Toggle";
 import { ThemeContext } from "./Context";
 function App() {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
   return (
-   
-  <div style={{backgroundColor:darkMode ? "#222": "white", color:darkMode && "white"}}>
-  <Toggle />
-    <Intro />
-    <About />
-    <ProductList />
-    <Contact />
 
-  </div>
-   
-   
-      
-    
-      
-  
+    <div style={{ backgroundColor: darkMode ? "#222" : "white", color: darkMode && "white" }}>
+      <Toggle />
+      <Intro />
+      <About />
+      <ProductList />
+      <Contact />
+    </div>
+
+
+
+
+
+
   );
 }
 
--- src/Context.js
+++ src/Context.js
@@ -2,22 +2,22 @@ import React from "react";
 import { createContext, useReducer } from "react";
 
 export const ThemeContext = createContext();
-const INITIAL_STATE = {darkMode:true};
+const INITIAL_STATE = { darkMode: true };
 
-const themeReducer = (state,action) => {
+const themeReducer = (state, action) => {
     switch (action.type) {
-        case "Toggle" :
-            return {darkMode: !state.darkMode};
-            default:
-                return state;
+        case "Toggle":
+            return { darkMode: !state.darkMode };
+        default:
+            return state;
     }
 }
 
-export const ThemeProvider = (props) =>{
-    const [state, dispatch] = useReducer(themeReducer,INITIAL_STATE)
+export const ThemeProvider = (props) => {
+    const [state, dispatch] = useReducer(themeReducer, INITIAL_STATE)
 
     return (
-        <ThemeContext.Provider value={{state, dispatch}}>
+        <ThemeContext.Provider value={{ state, dispatch }}>
             {props.children}
         </ThemeContext.Provider>
     )
